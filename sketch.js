4d9cf9212b5120804af96b44d4029d4cd47847ba
naygabys <gabriellefmendonca004@gmail.com>
2024-08-28 08:26:38 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,15 @@
+l>
+< html idioma = "pt" >    
+  < cabeça >  
+    < script src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/p5.js" > < / script >   
+    < script src = "https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/addons/p5.sound.min.js" >< / script >     
+    < link rel = "folha de estilo" tipo = "texto/css" href = "estilo.css" >              
+    < meta conjunto de caracteres = "utf-8" />         
+
+  < / cabeça >
+  < corpo >  
+    < principal >  
+    < / principal >
+    < script src = "sketch.js" >< / script >     
+  < / corpo >
+< / html >
